99c28b50ef54956098de0be2a738c7c72b05bd6d
AmandaKankkonen <amandakankkonen@gmail.com>
2022-02-07 14:50:39 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,99 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
-import Phonebook from './components/phonebook'
-import ClickCounter from './components/clickCounter'
-import Notification from './components/Notification'
 import noteService from './services/notes'
-import ReminderApp from './components/reminderApp'
+//import Phonebook from './components/phonebook'
+//import ClickCounter from './components/clickCounter'
+//import Notification from './components/Notification'
+//import ReminderApp from './components/reminderApp'
 
-const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+class App extends React.component {
 
-  useEffect(() => {
+  componentDidMount() {
     noteService
       .getAll()
-      .then(initialNotes => {
-      setNotes(initialNotes)
-    })
-  }, [])
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      note: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-    }
-
-    noteService
-      .create(noteObject)
-        .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
+      .then(response => {
+        this.setState({ notes: response })
       })
   }
 
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
+  addNote = (event) => {
+    // ...
     noteService
-    .update(id, changedNote)
-      .then(returnedNote => {
-      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-    })
-    .catch(error => {
-      setErrorMessage(
-        `Note '${note.content}' was already removed from server`
-      )
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    })    
+      .create(noteObject)
+      .then(newNote => {
+        this.setState({
+          notes: this.state.notes.concat(newNote),
+          newNote: ''
+        })
+      })
   }
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+  toggleImportanceOf = (id) => {
+    return () => {
+      // ...
+
+      noteService
+        .update(id, changedNote)
+        .then(changedNote => {
+          const notes = this.state.notes.filter(n => n.id !== id)
+          this.setState({
+            notes: notes.concat(changedNote)
+          })
+        })
+    }
   }
-
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
-
-  return (
-    <div>
-      <Phonebook />
-      <ClickCounter />
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>   
-      <ul>
-        {notesToShow.map(note => 
-            <Note
-              key={note.id}
-              note={note} 
-              toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">Save</button>
-      </form> 
-      <ReminderApp />
-    </div>
-  )
 }
-
-export default App
+ export default App
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,55 +1,19 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/reminders'
 
-const getAll2 = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const item = {
-    reminders: [
-      {
-        note: "Buy some eggs",
-        date: "2021-11-10T13:00:00.141Z",
-        id: 1,
-        important: true,
-      },
-      {
-        note: "Make an omelette",
-        date: "2021-11-11T08:00:00.141Z",
-        id: 2,
-        important: true,
-      },
-      {
-        note: "Wash dishes",
-        date: "2021-11-11T09:00:00.000Z",
-        id: 3,
-        important: true,
-      },
-      {
-        note: "Buy more eggs",
-        date: "2021-11-11T13:00:00.000Z",
-        id: 4,
-        important: true,
-      }
-    ],
-    newNote: ''
-  }
-  return request.then(response => response.data.concat(item))
+  return request.then(response => { return response.data })
 }
 
-const create = newObject => {
+const create = (newObject) => {
   const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+  return request.then(response => { return response.data })
 }
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+  return request.then(response => { return response.data })
 }
 
-export default { 
-  getAll, create, update
-}
+export default { getAll, create, update }
