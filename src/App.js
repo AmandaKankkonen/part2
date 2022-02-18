e7a2c3872673bc6c000ee3e2e2fd013f3aee0141
AmandaKankkonen <amandakankkonen@gmail.com>
2022-02-18 11:57:07 +0200

    Updates 18/02

--- src/App.js
+++ src/App.js
@@ -1,53 +1,105 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import axios from "axios"
+import reminderService from './services/reminders'
+//import React, { useState, useEffect } from 'react'
 //import Note from './components/Note'
-import noteService from './services/notes'
 //import Phonebook from './components/phonebook'
 //import ClickCounter from './components/clickCounter'
 //import Notification from './components/Notification'
 //import ReminderApp from './components/reminderApp'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reminders: [],
+      newNote: '',
+      newTime: ''
+    }
+  }
 
   componentDidMount() {
-    noteService
+    console.log('Mounted')
+    reminderService
       .getAll()
-      .then(response => {
-        this.setState({ notes: response })
+      .then(reminders => {
+        this.setState({ reminders })
       })
   }
 
-  addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: this.state.newNote,
-      date: new Date(),
-      important: Math.random() > 0.5
-    }
-  
+   addReminder = (event) =>{
+       event.preventDefault()
+       console.log("Add note")
+       const nameObject = {
+        note: this.state.newNote,
+        timestamp: this.state.newTime,
+      } 
+
+
     axios
-      .post('http://localhost:3001/reminders', noteObject)
-      .then(response => {
-        this.setState({
-          notes: this.state.notes.concat(response.data),
-          newNote: ''
-        })
+    .post('http://localhost:3001/reminders', nameObject)
+    .then(response => {
+      this.setState({
+        reminders: this.state.reminders.concat(response.data),
+        newNote: '',
+        newTime: ''
       })
+    })
+}
+
+   handleNoteChange = (event) => {
+       console.log(event.target.value)
+       this.setState({newNote: event.target.value})
+   }
+
+   handleTimeChange = (event) =>{
+     this.setState({newTime: event.target.value})
+   }
+
+   deleteReminder = (id) => {
+    return() => {
+    const url = `http://localhost:3001/reminders/${id}`
+
+    window.confirm("Are you sure you want to delete?") ?
+     axios
+     .delete(url)
+     (reminderService
+      .getAll()
+      .then(reminders => {
+        this.setState({ reminders })
+      })):
+     alert("Reminder was not deleted.");
+   } 
   }
 
-  toggleImportanceOf = (id) => {
-    return () => {
-      const url = 'http://localhost:3001/reminders/${id}'
-      const note = this.state.notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-  
-      axios
-        .put(url, changedNote)
-        .then(response => {
-          this.setState({
-            notes: this.state.notes.map(note => note.id !== id ? note : response.data)
-          })
-        })
-    }
+  render() {
+    return (
+      <div>
+         <h2>Add Reminder</h2>
+        <form onSubmit={this.addReminder}>
+        <div>
+            Subject: <input value={this.state.newNote} onChange={this.handleNoteChange} />
+        </div>
+        <div>
+            Time: <input value={this.state.newTime} onChange={this.handleTimeChange} />
+        </div>
+        <div>
+            <button type="submit">Add Reminder</button>
+        </div>
+        </form>
+        <h2>Reminders:</h2>
+        {this.state.reminders.map(reminder => { return (
+             <div>
+              <p>{reminder.timestamp} {reminder.note} 
+              <button onClick={this.deleteReminder(reminder.id)}>
+                Delete
+              </button></p>
+             </div>  
+        )})}
+      
+      </div>
+      
+    )
   }
 }
 
