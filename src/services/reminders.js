abcac3480f5b7008966c9e6498212c5b349aa36f
unknown <amandakankkonen@gmail.com>
2022-03-17 13:16:37 +0200

    Errors testing

--- src/services/reminders.js
+++ src/services/reminders.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/reminders'
 
 const getAll = () => {
-  const request = axios.get(baseUrl).catch(error => console.log(error.request));
+  const request = axios.get(baseUrl)
   return request.then(response => { return response.data })
 }
 
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject).catch(error => console.log(error.request));
+  const request = axios.post(baseUrl, newObject)
   return request.then(response => { return response.data })
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject).catch(error => console.log(error.request));
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => { return response.data })
 }
 
